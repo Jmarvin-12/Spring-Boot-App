f5cad82b6215365ef39f42f575f777163134e5ef
Marvin <josehenriquez.niu@gmail.com>
2020-07-18 03:10:44 -0600

    User and Role has been created

---
+++ SpringApp/src/main/java/com/mrh2020/entity/Role.java
@@ -0,0 +1,93 @@
+package com.mrh2020.entity;
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = 6940225779767103699L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native", strategy="native")
+	@Column
+	private int idRole;
+	@Column
+	private String name;
+	@Column
+	private String description;
+	
+	
+	public int getIdRole() {
+		return idRole;
+	}
+	
+	public void setIdRole(int idRole) {
+		this.idRole = idRole;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + idRole;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (idRole != other.idRole)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [idRole=" + idRole + ", name=" + name + ", description=" + description + "]";
+	}
+	
+}
